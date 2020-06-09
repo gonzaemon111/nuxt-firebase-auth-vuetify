96a063d644a7f8b7a479209bc831a37150906b36
gomzaemon111 <goki14914@icloud.com>
2020-06-10 00:22:02 +0900

    add pages/editor

---
+++ pages/editor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <editor v-model="editorText" :options="editorOptions" :visible="editorVisible" height="500px" previewStyle="vertical" />
+    <viewer :value="editorText" />
+  </div>
+
+</template>
+<script>
+  import {
+    Editor,
+    Viewer
+  } from '@toast-ui/vue-editor'
+
+  export default {
+    components: {
+      'editor': Editor,
+      'viewer': Viewer
+    },
+    data() {
+      return {
+        editorText: '',
+        editorOptions: {
+          hideModeSwitch: true
+        },
+        editorVisible: true
+      }
+    }
+  }
+</script>
